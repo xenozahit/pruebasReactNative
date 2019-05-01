cc3e3d34672f9faed32055f842b596a40984bebd
Zahit <zahit@xenotech.mx>
2019-05-01 16:58:12 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Home} from './app/views/Home.js';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export class Home extends React.Component{
+	render(){
+		return (
+			<View>
+				<Text>THE HOME PAGE</Text>
+				<Text>OTHER LINES</Text>
+				<Text>text in the app</Text>
+			</View>
+		);
+	}
+}
